6a4bef2024d0e42ef4b6d77604acbccad03ace80
LukeXF <me@luke.sx>
2014-07-01 22:37:05 +0100

    Added the one and only
    index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>Syclone Studios</title>
+
+<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+
+<style>
+	body {
+		padding-top: 250px;
+		background: url('http://luke.sx/ChromaDonateDev/assets/img/backdrop.png') no-repeat center center fixed;
+		-webkit-background-size: cover;
+  		-moz-background-size: cover;
+  		-o-background-size: cover;
+  		background-size: cover;
+		color: #fff;
+	}
+	h1, p, h3{
+		text-align: center;
+		font-family: Lato;
+	}
+	h3 {
+		margin-top: -20px;
+		font-size: 1000%;
+	}
+	h1 {
+		font-size: 500%;
+	}
+	p {
+		margin-top: -35px;
+		font-size: 300%;
+	}
+</style>
+
+
+</head>
+
+<body>
+	<h1>Welcome to Syclone Studios.</h1>
+	<p>Home of something extraordinary.</p>
+	<h3>;)</h3>
+
+</body>
+</html>
